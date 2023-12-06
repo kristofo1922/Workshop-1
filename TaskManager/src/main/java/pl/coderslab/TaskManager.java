011f0cd67ff825c7fc56f1ed0e8766d834f1f457
kristofo1922 <krzysztof.lubakow@gmail.com>
2023-12-06 22:25:02 +0100

    menu with choice

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,45 @@
 package pl.coderslab;
 
+
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+            useChance(menu);
+            input();
+        }
+
+    public static void useChance(String[] array) {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+            for (String option : array) {
+                System.out.println(option);
+            }
     }
+
+    public static void input() {
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.nextLine();
+
+        switch (scan) {
+            case "add" :
+                System.out.println("ADD");
+                break;
+            case "remove" :
+                System.out.println("REMOVE");
+                break;
+            case "list" :
+                System.out.println("LIST");
+                break;
+            case "exit" :
+                System.out.println(ConsoleColors.RED);
+                System.out.println("Koniec programu" + ConsoleColors.RESET);
+                break;
+            default:
+                System.out.println("Please select a corret option.");
+        }
+    }
+    //   static final String file = "tasks.csv";
+    static final String[] menu = {"add", "remove", "list" , "exit" };
 }
